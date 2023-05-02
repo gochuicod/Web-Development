10e975c2e0d6a0816d6ee5b942edb312617ac56d
gochuicod <gochuicodarelleq@gmail.com>
2023-05-02 21:46:21 +0800

    CRUD (FLASK) v2.2 | Added Confirmation Dialogs

--- CRUD (Flask)/static/home.js
+++ CRUD (Flask)/static/home.js
@@ -1,4 +1,10 @@
-function getCurrentStudent(data){
+let form = ''
+
+const getCurrentStudent = data => {
     document.querySelector(".student_fullname").innerHTML = `<b>Edit |</b> ${data[1]} ${data[2]}`
     document.querySelector(".student_id").setAttribute("value",data[0])
 }
+
+const transferValue = data => form = data
+
+document.querySelector('.confirmYes').addEventListener('click', () => document.querySelector(`.delete_${form}`).submit())
